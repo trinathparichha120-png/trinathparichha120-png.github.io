f044a7e5b2bba930c4d05a3857698aa8ea0a923e
Trinath Parichha <trinathparichha120@gmail.com>
2026-06-27 19:04:20 +0530

    Update data.js

--- data.js
+++ data.js
@@ -16,47 +16,51 @@ const syllabusDB = {
                 { name: '11. Dual Nature of Radiation and Matter', videoId: 'Y2BrCETu9EU' }, 
                 { name: '12. Atoms', videoId: 'uOaOviIfCTU' }, 
                 { name: '13. Nuclei', videoId: 'uOaOviIfCTU' }, 
-                { name: '14. Semiconductor Electronics', videoId: 'Y2BrCETu9EU' } 
+                { name: '14. Semiconductor Electronics', videoId: 'nCAXL800EZw' } 
             ],
             'chemistry': [
-                { name: '1. Solutions', videoId: 'rEUYpKt9V20' }, 
-                { name: '2. Electrochemistry', videoId: 'rEUYpKt9V20' },
-                { name: '3. Chemical Kinetics', videoId: 'rEUYpKt9V20' },
-                { name: '4. d and f Block Elements', videoId: 'rEUYpKt9V20' },
-                { name: '5. Coordination Compounds', videoId: 'rEUYpKt9V20' },
-                { name: '6. Haloalkanes and Haloarenes', videoId: 'rEUYpKt9V20' },
-                { name: '7. Alcohols, Phenols and Ethers', videoId: 'rEUYpKt9V20' },
-                { name: '8. Aldehydes, Ketones and Carboxylic Acids', videoId: 'rEUYpKt9V20' },
-                { name: '9. Amines', videoId: 'rEUYpKt9V20' },
-                { name: '10. Biomolecules', videoId: 'rEUYpKt9V20' }
+                { name: '1. Solutions', videoId: 'xw3C6_ZDDBo' }, 
+                { name: '2. Electrochemistry', videoId: 'nPQ-mvIl_bQ' },
+                { name: '3. Chemical Kinetics', videoId: 'WuARVOxfRNQ' },
+                { name: '4. d and f Block Elements', videoId: 'ZHdxNDnJf4o' },
+                { name: '5. Coordination Compounds', videoId: 'EszE3aP4yIM' },
+                { name: '6. Haloalkanes and Haloarenes', videoId: 'iAwju2Xk2xc' },
+                { name: '7. Alcohols, Phenols and Ethers', videoId: 'yzRH8PTJzkM' },
+                { name: '8. Aldehydes, Ketones and Carboxylic Acids', videoId: 'HB8CZzDnKHI' },
+                { name: '9. Amines', videoId: 'lQbgRJnVShY' },
+                { name: '10. Biomolecules', videoId: 'qew8jac0z7g' }
             ],
             'mathematics': [
-                { name: '1. Relations and Functions', videoId: 'uTx3zRNqwwM' }, 
-                { name: '2. Inverse Trigonometric Functions', videoId: 'uTx3zRNqwwM' }, 
-                { name: '3. Matrices', videoId: 'uTx3zRNqwwM' }, 
-                { name: '4. Determinants', videoId: 'uTx3zRNqwwM' }, 
-                { name: '5. Continuity and Differentiability', videoId: 'uTx3zRNqwwM' }, 
-                { name: '6. Application of Derivatives', videoId: 'uTx3zRNqwwM' }, 
-                { name: '7. Integrals', videoId: 'uTx3zRNqwwM' }, 
-                { name: '8. Application of Integrals', videoId: 'uTx3zRNqwwM' }, 
-                { name: '9. Differential Equations', videoId: 'uTx3zRNqwwM' }, 
-                { name: '10. Vector Algebra', videoId: 'uTx3zRNqwwM' }, 
-                { name: '11. Three Dimensional Geometry', videoId: 'uTx3zRNqwwM' }, 
-                { name: '12. Linear Programming', videoId: 'uTx3zRNqwwM' }, 
-                { name: '13. Probability', videoId: 'uTx3zRNqwwM' } 
+                { name: '1. Relations and Functions', videoId: 'zyFucmwc2gY' }, 
+                { name: '2. Inverse Trigonometric Functions', videoId: 'Oy-Z49idgVw' }, 
+                { name: '3. Matrices', videoId: 'MYF034ZUKxo' }, 
+                { name: '4. Determinants', videoId: '2NncYZhn6FI' }, 
+                { name: '5. Continuity and Differentiability', videoId: 'cnpv-t2DGiw' }, 
+                { name: '6. Application of Derivatives', videoId: 'fy4m6lZ9YwA' }, 
+                { name: '7. Integrals', videoId: 'yY2oUKVAkdY' }, 
+                { name: '8. Application of Integrals', videoId: 'lnR343-9vFY' }, 
+                { name: '9. Differential Equations', videoId: '4r-0JSfGez4' }, 
+                { name: '10. Vector Algebra', videoId: 'U7nSbEZyYnA' }, 
+                { name: '11. Three Dimensional Geometry', videoId: 'sNG2FVZi5d8' }, 
+                { name: '12. Linear Programming', videoId: '0bVrrnAjAyw' }, 
+                { name: '13. Probability', videoId: 'I4WvS7_rk8k' } 
             ],
-            'biology': [
-                { name: '1. Reproduction in Organisms', videoId: 'oF4xQe3u92E' },
-                { name: '2. Human Reproduction', videoId: 'oF4xQe3u92E' },
-                { name: '3. Genetics', videoId: 'oF4xQe3u92E' }
+            // Botany - Vipin Sir One Shots
+            'botany': [
+                { name: '1. Reproduction in Organisms (One Shot)', videoId: 'LmRldTCcf1w' },
+                { name: '2. Sexual Reproduction in Flowering Plants', videoId: 'AT_Jqc-E1Eg' },
+                { name: '8. Mendelian Genetics (One Shot)', videoId: 'nyIf7AXZ4I8' },
+                { name: '9. Molecular Basis of Inheritance (One Shot)', videoId: 'bptWgjQ_jFA' }
             ],
-            'english': [
-                { name: '1. Reading Comprehension', videoId: 'hQ6-B7R4Yiw' },
-                { name: '2. Literature', videoId: 'hQ6-B7R4Yiw' }
-            ],
-            'compsci': [
-                { name: '1. Python Fundamentals', videoId: 'Y8Tko2YAui0' },
-                { name: '2. Database Concepts', videoId: 'Y8Tko2YAui0' }
+            // Zoology - Vipin Sir One Shots
+            'zoology': [
+                { name: '1. Human Reproduction (One Shot)', videoId: 'zZ05EByJ9ts' },
+                { name: '2. Reproductive Health (One Shot)', videoId: '48gImtbYLsQ' },
+                { name: '3. Evolution (One Shot)', videoId: 'UhNT6wM9sng' },
+                { name: '4. Human Health and Disease (One Shot)', videoId: 'uySMYvvV1H4' },
+                { name: '5. Biotechnology: Principles and Processes', videoId: 'PqzJfOddRWE' },
+                { name: '6. Biotechnology and its Applications', videoId: 'M-H9I027lAI' },
+                { name: '7. Ecology (One Shot)', videoId: 'Sydlo51d8QA' }
             ]
         }
     }
@@ -74,47 +78,22 @@ delete syllabusDB['plus2-2nd']['chse']['english'];
 
 // 4. Overwrite Arrays with Verified "One Shot" Data
 
-// Chemistry - Ashu Sir One Shots
-syllabusDB['plus2-2nd']['chse']['chemistry'] = [
-    { name: '1. Solutions (One Shot)', videoId: '50U_ZfcrzuI' }, 
-    { name: '2. Electrochemistry (One Shot)', videoId: '50U_ZfcrzuI' },
-    { name: '3. Chemical Kinetics (One Shot)', videoId: '50U_ZfcrzuI' },
-    { name: '4. d and f Block Elements (One Shot)', videoId: '50U_ZfcrzuI' },
-    { name: '5. Coordination Compounds (One Shot)', videoId: '50U_ZfcrzuI' },
-    { name: '6. Haloalkanes and Haloarenes (One Shot)', videoId: '50U_ZfcrzuI' },
-    { name: '7. Alcohols, Phenols and Ethers (One Shot)', videoId: '50U_ZfcrzuI' },
-    { name: '8. Aldehydes, Ketones and Carboxylic Acids', videoId: '50U_ZfcrzuI' },
-    { name: '9. Amines (One Shot)', videoId: '50U_ZfcrzuI' },
-    { name: '10. Biomolecules (One Shot)', videoId: '50U_ZfcrzuI' }
-];
 
-// Botany - Vipin Sir One Shots
+
+// Botany - Vipin Sir One Shots class 11th
 syllabusDB['plus2-2nd']['chse']['botany'] = [
-    { name: '1. Reproduction in Organisms (One Shot)', videoId: '3o54J5R0nlQ' },
-    { name: '2. Sexual Reproduction in Flowering Plants', videoId: '3o54J5R0nlQ' },
-    { name: '3. Plant Water Relations (One Shot)', videoId: '3o54J5R0nlQ' },
-    { name: '4. Mineral Nutrition (One Shot)', videoId: '3o54J5R0nlQ' },
-    { name: '5. Photosynthesis (One Shot)', videoId: '3o54J5R0nlQ' },
-    { name: '6. Respiration (One Shot)', videoId: '3o54J5R0nlQ' },
-    { name: '7. Plant Growth & Development (One Shot)', videoId: '3o54J5R0nlQ' },
-    { name: '8. Mendelian Genetics (One Shot)', videoId: '3o54J5R0nlQ' },
-    { name: '9. Molecular Basis of Inheritance (One Shot)', videoId: '3o54J5R0nlQ' }
+    { name: '3. Plant Water Relations (One Shot)', videoId: 'uubHmiafxG8'},
+    { name: '4. Mineral Nutrition (One Shot)', videoId: 'g4_7y9R0bKM' },
+    { name: '5. Photosynthesis (One Shot)', videoId: 'orPhMWc0CMw' },
+    { name: '6. Respiration (One Shot)', videoId: 'kC0hk6_Uxis' },
+    { name: '7. Plant Growth & Development (One Shot)', videoId: '5rhSaeXug2k' },
 ];
 
-// Zoology - Vipin Sir One Shots
-syllabusDB['plus2-2nd']['chse']['zoology'] = [
-    { name: '1. Human Reproduction (One Shot)', videoId: 'r3_N_9Dtljk' },
-    { name: '2. Reproductive Health (One Shot)', videoId: 'r3_N_9Dtljk' },
-    { name: '3. Evolution (One Shot)', videoId: 'r3_N_9Dtljk' },
-    { name: '4. Human Health and Disease (One Shot)', videoId: 'r3_N_9Dtljk' },
-    { name: '5. Biotechnology: Principles and Processes', videoId: 'r3_N_9Dtljk' },
-    { name: '6. Biotechnology and its Applications', videoId: 'r3_N_9Dtljk' },
-    { name: '7. Ecology (One Shot)', videoId: 'r3_N_9Dtljk' }
-];
+// Zoology - Vipin Sir One Shots class 11th
 
 // MIL Odia - HK Sir One Shots
 syllabusDB['plus2-2nd']['chse']['mil-odia'] = [
-    { name: 'Prose: Itihasa (One Shot)', videoId: 'z1MLMknyWMQ' },
+    { name: 'Prose: Itihasa (One Shot)', videoId: 'GKWJzuRVQrk' },
     { name: 'Prose: Pushpapura re Varshabarana', videoId: 'z1MLMknyWMQ' },
     { name: 'Prose: Swadhina Deshare Sikshya Chinta', videoId: 'z1MLMknyWMQ' },
     { name: 'Poetry: Bada Pana (One Shot)', videoId: 'z1MLMknyWMQ' },
@@ -132,32 +111,32 @@ syllabusDB['plus2-2nd']['chse']['mil-odia'] = [
 
 // English Invitations - One Shots
 syllabusDB['plus2-2nd']['chse']['english-inv1'] = [
-    { name: 'Prose: My Greatest Olympic Prize', videoId: 'MRsMxz91UKE' },
-    { name: 'Prose: On Examinations', videoId: 'MRsMxz91UKE' },
-    { name: 'Prose: The Magic of Teamwork', videoId: 'MRsMxz91UKE' },
-    { name: 'Poetry: Daffodils', videoId: 'MRsMxz91UKE' },
-    { name: 'Poetry: The Ballad of Father Gilligan', videoId: 'MRsMxz91UKE' },
-    { name: 'Poetry: A Psalm of Life', videoId: 'MRsMxz91UKE' }
+    { name: 'Prose: My Greatest Olympic Prize', videoId: 'Y7w1EVjG0aY' },
+    { name: 'Prose: On Examinations', videoId: 'Y7w1EVjG0aY' },
+    { name: 'Prose: The Magic of Teamwork', videoId: 'Y7w1EVjG0aY' },
+    { name: 'Poetry: Daffodils', videoId: 'Y7w1EVjG0aY' },
+    { name: 'Poetry: The Ballad of Father Gilligan', videoId: 'Y7w1EVjG0aY' },
+    { name: 'Poetry: A Psalm of Life', videoId: 'Y7w1EVjG0aY' }
 ];
 
 syllabusDB['plus2-2nd']['chse']['english-inv2'] = [
-    { name: '1. The Doctor\'s Word', videoId: 'hiKzGhc9i5Y' },
-    { name: '2. The Nightingale and the Rose', videoId: 'hiKzGhc9i5Y' },
-    { name: '3. Mystery of the Missing Cap', videoId: 'hiKzGhc9i5Y' },
-    { name: '4. The Monkey\'s Paw', videoId: 'hiKzGhc9i5Y' }
+    { name: '1. The Doctor\'s Word', videoId: 'Y7w1EVjG0aY' },
+    { name: '2. The Nightingale and the Rose', videoId: 'Y7w1EVjG0aY' },
+    { name: '3. Mystery of the Missing Cap', videoId: 'Y7w1EVjG0aY' },
+    { name: '4. The Monkey\'s Paw', videoId: 'Y7w1EVjG0aY' }
 ];
 
 syllabusDB['plus2-2nd']['chse']['english-inv3'] = [
-    { name: '1. Essay Writing', videoId: 'MRsMxz91UKE' },
-    { name: '2. Report Writing', videoId: 'MRsMxz91UKE' },
-    { name: '3. Note Making', videoId: 'MRsMxz91UKE' }
+    { name: '1. Essay Writing', videoId: 'Y7w1EVjG0aY' },
+    { name: '2. Report Writing', videoId: 'Y7w1EVjG0aY' },
+    { name: '3. Note Making', videoId: 'Y7w1EVjG0aY' }
 ];
 
 syllabusDB['plus2-2nd']['chse']['english-inv4'] = [
-    { name: '1. Tense Patterns', videoId: 'hiKzGhc9i5Y' },
-    { name: '2. Modals', videoId: 'hiKzGhc9i5Y' },
-    { name: '3. Conditionals', videoId: 'hiKzGhc9i5Y' },
-    { name: '4. Passive Voice', videoId: 'hiKzGhc9i5Y' }
+    { name: '1. Tense Patterns', videoId: 'Y7w1EVjG0aY' },
+    { name: '2. Modals', videoId: 'Y7w1EVjG0aY' },
+    { name: '3. Conditionals', videoId: 'Y7w1EVjG0aY' },
+    { name: '4. Passive Voice', videoId: 'Y7w1EVjG0aY' }
 ];
 
 // 5. Appending IT Module
@@ -171,30 +150,30 @@ syllabusDB['plus2-2nd']['chse']['it'] = [
 
 // 6. Retain other CHSE Specific Subjects
 syllabusDB['plus2-2nd']['chse']['compsci'] = [
-    { name: '1. Python Revision Tour', videoId: 'Y8Tko2YAui0' },
-    { name: '2. Functions & Modules', videoId: 'Y8Tko2YAui0' },
-    { name: '3. File Handling', videoId: 'Y8Tko2YAui0' },
-    { name: '4. Data Structures (Stacks)', videoId: 'Y8Tko2YAui0' },
-    { name: '5. Computer Networks', videoId: 'Y8Tko2YAui0' },
-    { name: '6. Database Management (SQL)', videoId: 'Y8Tko2YAui0' },
-    { name: '7. Interface Python with MySQL', videoId: 'Y8Tko2YAui0' }
+    { name: '1. Python Revision Tour', videoId: '' },
+    { name: '2. Functions & Modules', videoId: '' },
+    { name: '3. File Handling', videoId: '' },
+    { name: '4. Data Structures (Stacks)', videoId: '' },
+    { name: '5. Computer Networks', videoId: '' },
+    { name: '6. Database Management (SQL)', videoId: '' },
+    { name: '7. Interface Python with MySQL', videoId: '' }
 ];
 
 syllabusDB['plus2-2nd']['chse']['statistics'] = [
-    { name: '1. Probability Theory', videoId: '9b8Dhh-Pq1w' },
-    { name: '2. Random Variables', videoId: '9b8Dhh-Pq1w' },
-    { name: '3. Theoretical Distributions', videoId: '9b8Dhh-Pq1w' },
-    { name: '4. Statistical Inference', videoId: '9b8Dhh-Pq1w' },
-    { name: '5. Time Series Analysis', videoId: '9b8Dhh-Pq1w' }
+    { name: '1. Probability Theory', videoId: '' },
+    { name: '2. Random Variables', videoId: '' },
+    { name: '3. Theoretical Distributions', videoId: '' },
+    { name: '4. Statistical Inference', videoId: '' },
+    { name: '5. Time Series Analysis', videoId: '' }
 ];
 
 syllabusDB['plus2-2nd']['chse']['electronics'] = [
-    { name: '1. Semiconductor Physics', videoId: 'wNJTLxXfDS8' },
-    { name: '2. Junction Diodes', videoId: 'wNJTLxXfDS8' },
-    { name: '3. Bipolar Junction Transistors', videoId: 'wNJTLxXfDS8' },
-    { name: '4. Amplifiers', videoId: 'wNJTLxXfDS8' },
-    { name: '5. Oscillators', videoId: 'wNJTLxXfDS8' },
-    { name: '6. Digital Electronics Basics', videoId: 'wNJTLxXfDS8' }
+    { name: '1. Semiconductor Physics', videoId: '' },
+    { name: '2. Junction Diodes', videoId: '' },
+    { name: '3. Bipolar Junction Transistors', videoId: '' },
+    { name: '4. Amplifiers', videoId: '' },
+    { name: '5. Oscillators', videoId: '' },
+    { name: '6. Digital Electronics Basics', videoId: '' }
 ];
 
 syllabusDB['plus2-2nd']['chse']['history'] = [
